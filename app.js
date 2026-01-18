072246d802170ac57445fb023376e5d5fe6f06cd
Mayka2 <mayka_.1993@hotmail.com>
2026-01-18 12:02:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+
+// Creamos la variable donde se va a guardar la clave real
+
+let claveReal = "";
+
+// Creamos un contenedor con estilos básicos, que añadiremos al body con appenchild
+
+const contenedor = document.createElement("div");
+contenedor.style.maxWidth = "220px";
+contenedor.style.maxHeight = "370px";
+contenedor.style.margin = "20px auto";
+contenedor.style.textAlign = "center";
+contenedor.style.border = "9px solid #000000";
+contenedor.style.borderRadius="8px";
+contenedor.style.backgroundColor = "#c6d5f4";
+document.body.appendChild(contenedor);
+
+// Creamos el campo para visualizar la clave oculta y lo metemos en el contenedor
+const inputOculto = document.createElement("input");
+inputOculto.type = "password";
+inputOculto.readOnly = true;
+inputOculto.style.marginTop = "2px";
+inputOculto.style.width = "145px";
+inputOculto.style.height = "50px";
+inputOculto.style.border = "5px solid #333333";
+inputOculto.style.borderRadius="8px";
+inputOculto.style.display = "block";
+inputOculto.style.margin = "15px 0 5px 20px";
+inputOculto.style.fontSize = "30px";
+
+
+
+contenedor.appendChild(inputOculto);
+
+
+
+
+// Creamos el contenedor para los botones
+const contenedorBotones = document.createElement("div");
+contenedor.appendChild(contenedorBotones);
+
+// Creamos un array de números
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numeros.sort(() => Math.random() - 0.5);
+
+// Recorremos el array números
+numeros.forEach(num => {
+    // Creamos el botón con el número y sus caracteristicas
+    const btn = document.createElement("button");
+    btn.textContent = num;
+    btn.style.width = "50px";
+    btn.style.height = "50px";
+    btn.style.margin = "7px";
+    btn.style.border = "5px solid #333333"; 
+  
+    
+    btn.style.borderRadius="8px";
+    btn.style.backgroundColor = "#6094fb";
+    btn.style.fontSize = "30px";
+    
+
+    btn.addEventListener("click", () => {
+        if (claveReal.length < 4) {          // máximo 4 dígitos
+            claveReal += String(num);          // añadimos el número
+            inputOculto.value = claveReal;     // el input password la oculta
+            divMensajes.textContent = "";      // limpiamos mensajes
+        }
+    });
+
+    // Metemos el botón en el contenedor de botones
+    contenedorBotones.appendChild(btn);
+});
+contenedorBotones.appendChild(document.createElement("br"));
+
+// Creamos el botón C
+const btnC = document.createElement("button");
+btnC.textContent = "C";
+btnC.style.width = "40px";
+btnC.style.height = "50px";
+btnC.style.margin = "5px";
+btnC.style.fontSize = "30px";
+btnC.style.border = "5px solid #333333";
+btnC.style.borderRadius="8px";
+
+// Creamos la función del botón C, borrar si la clave real no está vacia
+btnC.addEventListener("click", () => {
+    if (claveReal.length > 0) {
+        claveReal = claveReal.slice(0, -1);
+        inputOculto.value = claveReal; // se actualiza y sigue oculto
+        divMensajes.textContent = "";
+    }
+});
+// Metemos el botón en el contenedor de botones
+contenedorBotones.appendChild(btnC);
+
+// Creamos el botón validar
+const btnValidar = document.createElement("button");
+btnValidar.textContent = "Validar";
+btnValidar.style.width = "120px";
+btnValidar.style.height = "50px";
+btnValidar.style.margin = "5px";
+btnValidar.style.fontSize = "30px";
+btnValidar.style.border = "5px solid #333333";
+btnValidar.style.borderRadius="8px";
+
+// Creamos la función del botón validar
+btnValidar.addEventListener("click", () => {
+    if (claveReal.length !== 4) {
+        divMensajes.textContent = "La clave debe tener exactamente 4 caracteres.";
+        divMensajes.style.color = "red";
+        return;
+    }
+
+    // Comprobar que la clave es 9999 con regex
+    const regex = /^9999$/;   // solo acepta "9999"
+
+    if (regex.test(claveReal)) {
+        divMensajes.textContent = "Contraseña correcta. Acceso permitido.";
+        divMensajes.style.color = "green";
+    } else {
+        divMensajes.textContent = "Contraseña incorrecta. Inténtelo de nuevo.";
+        divMensajes.style.color = "red";
+    }
+});
+contenedorBotones.appendChild(btnValidar);
+
+// Div para mensajes
+const divMensajes = document.createElement("div");
+divMensajes.style.marginTop = "10px";
+divMensajes.style.marginBottom = "10px";
+
+contenedor.appendChild(divMensajes);
+
---
+++ primitiva.js
@@ -0,0 +1,168 @@
+
+// Creamos el contenedor donde irá el título
+const contenedor = document.createElement('div');
+contenedor.style.width = '310px';
+contenedor.style.margin = '5px auto';
+contenedor.style.border = "3px solid #b10202";
+contenedor.style.borderRadius = "5px";
+document.body.appendChild(contenedor);
+
+// Creamos el titulo
+const titulo = document.createElement('h5');
+titulo.textContent = 'LOTERIA PRIMITIVA.';
+titulo.style.textAlign = 'left';
+titulo.style.margin = '5px 5px 5px 10px';
+contenedor.appendChild(titulo);
+
+// Creamos el contenedor para los números
+const grid = document.createElement('div');
+grid.style.display = 'grid';
+grid.style.width = '295px';
+grid.style.margin = '5px auto';
+grid.style.gridTemplateColumns = 'repeat(7, 1fr)';
+grid.style.gap = '10px';
+grid.style.padding = "7px";
+grid.style.border = "3px solid #b10202";
+grid.style.borderRadius = "5px";
+document.body.appendChild(grid);
+
+// Array donde guardaremos los números seleccionados
+let seleccionados = [];
+
+// Recorremos del 1 al 49 creando un botón por cada número
+for (let n = 1; n <= 49; n++) {
+    const casilla = document.createElement("button");
+    casilla.textContent = n;
+    casilla.dataset.numero = n;
+    casilla.style.width = '30px';
+    casilla.style.height = '30px';
+    casilla.style.borderRadius = '5px';
+    casilla.style.backgroundColor = '#ffffff';
+    casilla.style.color = '#000000';
+    casilla.style.fontSize = '16px';
+    casilla.style.textAlign = 'center';
+    casilla.style.display = 'flex';
+    casilla.style.justifyContent = 'center';
+    casilla.style.alignItems = 'center';
+    casilla.style.cursor = 'pointer';
+
+
+    // marcar o desmarcar al hacer click
+    casilla.addEventListener("click", () => {
+        const num = Number(casilla.dataset.numero);
+        const indice = seleccionados.indexOf(num);
+
+        if (indice === -1) {
+            // aún no está seleccionado
+            if (seleccionados.length >= 6) {
+                alert("Solo puedes marcar 6 números.");
+                return;
+            }
+            seleccionados.push(num);
+            casilla.style.color = '#ff1919'; // marcado
+            casilla.style.fontWeight = 'bold';
+        } else {
+            // ya estaba → desmarcar
+            seleccionados.splice(indice, 1);
+            casilla.style.color = "#000000"; // vuelve a negro
+        }
+    });
+    grid.appendChild(casilla);
+}
+
+// Creamos el contenedor para botón y resultados de números ganadores
+const contenedorBoton = document.createElement('div');
+contenedorBoton.style.marginTop = '10px';
+contenedorBoton.style.padding = '5px';
+contenedorBoton.style.border = "3px solid #b10202";
+contenedorBoton.style.borderRadius = "5px";
+contenedorBoton.style.width = '300px';
+contenedorBoton.style.margin = '5px auto';
+contenedorBoton.style.display = 'flex';
+contenedorBoton.style.alignItems = 'center';
+contenedorBoton.style.gap = '5px';
+document.body.appendChild(contenedorBoton);
+
+
+// Creamos el botón de Realizar sorteo
+const btnSorteo = document.createElement('button');
+btnSorteo.textContent = 'Realizar sorteo';
+btnSorteo.style.width = '55px';
+btnSorteo.style.height = '40px';
+btnSorteo.style.borderRadius = '3px';
+btnSorteo.style.backgroundColor = '#ecebeb';
+btnSorteo.style.fontSize = '10px';
+btnSorteo.style.textAlign = 'center';
+btnSorteo.style.display = 'flex';
+btnSorteo.style.justifyContent = 'center';
+btnSorteo.style.alignItems = 'center';
+btnSorteo.style.cursor = 'pointer';
+contenedorBoton.appendChild(btnSorteo);
+
+// Al pulsar el botón se valida, se genera el sorteo y se muestran los resultados
+btnSorteo.addEventListener("click", () => {
+    // 1) validar que haya 6 marcados
+    if (seleccionados.length !== 6) {
+        mensajeAciertos.textContent = "Debes marcar exactamente 6 números.";
+        mensajeAciertos.style.color = "red";
+        return;
+    }
+
+    // 2) Generar sorteo
+    const ganadores = generarSorteo();
+
+    // 3) Mostrar los números ganadores
+    resultadoSorteo.textContent = '';
+    ganadores.forEach(n => {
+        const b = document.createElement("button");
+        b.textContent = n;
+        b.style.width = "30px";
+        b.style.height = "30px";
+        b.style.margin = "3px";
+        b.style.borderRadius = "5px";
+        b.style.backgroundColor = "#ffffff";
+        b.style.fontSize = "16px";
+        resultadoSorteo.appendChild(b);
+    });
+    // 4)  Contar cuántos aciertos hay
+    let aciertos = 0;
+    seleccionados.forEach(num => {
+        if (ganadores.includes(num)) aciertos++;
+    });
+    // 5) Mostrar los aciertos
+    mensajeAciertos.textContent = "Has tenido " + aciertos + " aciertos.";
+    mensajeAciertos.style.color = aciertos > 0 ? "green" : "black";
+});
+
+// Donde mostraremos los 6 números ganadores
+const resultadoSorteo = document.createElement("div");
+resultadoSorteo.style.marginTop = "8px";
+resultadoSorteo.style.gap = "5px";
+resultadoSorteo.style.display = "flex";
+contenedorBoton.appendChild(resultadoSorteo);
+
+// Cuadro de mensaje de aciertos
+const mensajeAciertos = document.createElement("div");
+mensajeAciertos.style.marginTop = "8px";
+mensajeAciertos.style.padding = "5px";
+mensajeAciertos.style.border = "3px solid #b10202";
+mensajeAciertos.style.borderRadius = "5px";
+mensajeAciertos.style.width = "300px";
+mensajeAciertos.style.margin = "5px auto";
+document.body.appendChild(mensajeAciertos);
+
+// Función para generar el sorteo
+function generarSorteo() {
+    const numeros = [];
+    // Mientras no tengamos 6 numeros
+    while (numeros.length < 6) {
+        // Generamos un número aleatorio
+        const n = Math.floor(Math.random() * 49) + 1; // 1..49
+        // Si no hemos generado ese número, lo metemos
+        if (!numeros.includes(n)) {
+            numeros.push(n);
+        }
+    }
+    // Ordenamos los números
+    return numeros.sort((a, b) => a - b);
+}
